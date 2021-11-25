3288b50777c83422fa4ef6c09ee35d2028eaf887
Mandy_Mascetti <armascetti@gmail.com>
2021-11-24 19:54:46 -0500

    associate M:M model

--- main_app/models.py
+++ main_app/models.py
@@ -26,6 +26,7 @@ class Finch(models.Model):
   species = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
 
 def fed_for_today(self):
   return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
